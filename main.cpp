813ec7ee95586fa382d90b8194e2b661da52ea17
imane-jourji <imanejourji@gmail.com>
2024-08-30 16:17:58 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* My name : Imane Jourji 
+   Computer Science Fall 2024
+   Due : 09/03/2024 
+   Lab : Lab1 Introduction to C++
+   Demonstration of the basics of C++
+
+   */
+#include<iostream>
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Pink" << std::endl;
+
+	return 0;
+}
